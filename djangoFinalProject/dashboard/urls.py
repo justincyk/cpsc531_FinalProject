85abb94bb36ec054783ff69ea28d9059ba80acf9
justincyk <jcykim@csu.fullerton.edu>
2023-05-01 02:23:40 -0700

    created new branch:

--- djangoFinalProject/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('report/mushrooms', views.mushrooms_report_page, name='mushrooms_report'),
-]
--- djangoFinalProject/dashboard/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def mushrooms_report_page(request):
-   return render(request, 'mushrooms_report.html', {})
